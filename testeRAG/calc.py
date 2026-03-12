250c8e9d8b8fdf9ab5ebb444c2b82a0bb8b0eed1
Paula Soares <sou@paulasoares.me>
2026-03-12 18:21:19 -0300

    ingestão de código

---
+++ testeRAG/calc.py
@@ -0,0 +1,13 @@
+# Módulo de cálculos
+def soma(a, b):
+    # Falta docstring e tipagem
+    return a + b
+
+def processa_dados(lista):
+    # Função muito longa e sem logging
+    resultado = 0
+    for i in lista:
+        resultado += i
+    # Senha hardcoded (violação de segurança)
+    senha = "123456" 
+    return resultado
